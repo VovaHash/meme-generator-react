3b20226f8b62f2176917fdd77003aa8c6bb872e9
VovaHash <vovahash01@gmail.com>
2022-06-19 23:18:40 +0300

    header

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Headers(){
+    return(
+        <div className="header">
+            <img className="header--image"  src="./images/Troll Face.svg" />
+            <h1 className="header--title">Meme Generator</h1>
+            <h3 className="header--subtitle">React Course - Project 3</h3>
+        </div>
+    )
+}
