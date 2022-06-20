d14796bdffa88300c234325dda51e0b9828aaa46
VovaHash <vovahash01@gmail.com>
2022-06-21 00:44:49 +0300

    header and input form

---
+++ src/components/Meme.js
@@ -0,0 +1,11 @@
+export default function Meme() {
+  return (
+    <>
+      <div className="meme--inputs_wrap">
+        <input className="meme--inputs" type="text"/>
+        <input className="meme--inputs" type="text"/>
+      </div>
+      <a className="meme--button">Get a new meme image  🖼</a>
+    </>
+  );
+}
