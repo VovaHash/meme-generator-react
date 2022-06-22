5484819b6999321777586794b400c8e13f073007
VovaHash <vovahash01@gmail.com>
2022-06-22 09:33:48 +0300

    assign random url from an array to a button click

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,26 @@
+import memesData from '../memesData'
+
+
+
 export default function Meme() {
+  function getMemeImage(){
+    const memesArray = memesData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    // or we could destructure it like   const {url} = memesArray[randomNumber]
+    console.log(url);
+  }
+
+
   return (
     <>
-      <form className="form">
+      <div className="form">
         <div className="form--inputs_wrap">
           <input className="form--input" type="text" placeholder="Top text"/>
           <input className="form--input" type="text" placeholder="Bottom text"/>
         </div>
-        <button className="form--button">Get a new meme image  🖼</button>
-      </form>
+        <button className="form--button" onClick={getMemeImage} >Get a new meme image  🖼</button>
+      </div>
     </>
   );
 }
