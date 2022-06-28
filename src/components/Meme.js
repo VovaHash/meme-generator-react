51f6f1c97b181a3f2fdead9c82f8dbff759cc78a
VovaHash <vovahash01@gmail.com>
2022-06-28 23:20:56 +0300

    implementing state little by little

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,21 @@
 import memesData from '../memesData'
+import React from 'react';
 
 
 
 export default function Meme() {
+
+  const [memeImage,setMemeImage] = React.useState('');
+
   function getMemeImage(){
     const memesArray = memesData.data.memes;
     const randomNumber = Math.floor(Math.random() * memesArray.length);
     const url = memesArray[randomNumber].url;
     // or we could destructure it like   const {url} = memesArray[randomNumber]
-    console.log(url);
+    setMemeImage(url);
   }
+  
+  
 
 
   return (
@@ -20,7 +26,9 @@ export default function Meme() {
           <input className="form--input" type="text" placeholder="Bottom text"/>
         </div>
         <button className="form--button" onClick={getMemeImage} >Get a new meme image  🖼</button>
+       
       </div>
+      <img className="meme-image" src={memeImage} alt='meme image'/>
     </>
   );
 }
